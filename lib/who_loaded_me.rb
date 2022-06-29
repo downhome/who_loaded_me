26b2808d5e59015fab5b697077ad4be05f627759
Maher Hawash <gmhawash@gmail.com>
2022-06-29 11:45:23 -0700

    Add comment

--- lib/who_loaded_me.rb
+++ lib/who_loaded_me.rb
@@ -1,5 +1,12 @@
+# frozen_string_literal: true
+#
 module WhoLoadedMe
   module_function
+  # Keeps track of the apps who loaded the specific gem with timestamp, and pwd
+  # #params
+  #   gem_name: name of gem that wants to be tracked
+  #   track_file: name of file where tracks are kept
+  #
   def track!(gem_name, track_file: nil)
     track_file ||= '/tmp/who_loaded_me.txt'
     File.open(track_file, 'a') do |f|
