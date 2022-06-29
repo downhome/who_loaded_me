603b2527a8870f173ddacc6f3015856073811444
Maher Hawash <gmhawash@gmail.com>
2022-06-29 10:46:18 -0700

    Append to file.

--- lib/who_loaded_me.rb
+++ lib/who_loaded_me.rb
@@ -3,8 +3,8 @@ module WhoLoadedMe
   module_function
   def track!(track_file = nil)
     track_file ||= '/tmp/who_loaded_me.txt'
-    File.open(track_file, 'w') do |f|
-      f.write("[%s]: App: [%s]: PWD: [%s]]" % [Time.now, $0, ENV['PWD']])
+    File.open(track_file, 'a') do |f|
+      f.puts("[%s]: App: [%s]: PWD: [%s]]" % [Time.now, $0, ENV['PWD']])
     end
   end
 end
