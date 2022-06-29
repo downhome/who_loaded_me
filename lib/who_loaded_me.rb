0bd9b81c7e1d31459a13d7399cb8b48f7069da3c
Maher Hawash <gmhawash@gmail.com>
2022-06-29 11:39:45 -0700

    Specify gem name

--- lib/who_loaded_me.rb
+++ lib/who_loaded_me.rb
@@ -1,12 +1,11 @@
 module WhoLoadedMe
-
   module_function
-  def track!(track_file = nil)
+  def track!(gem_name, track_file: nil)
     track_file ||= '/tmp/who_loaded_me.txt'
     File.open(track_file, 'a') do |f|
-      f.puts("[%s]: App: [%s]: PWD: [%s]]" % [Time.now, $0, ENV['PWD']])
+      f.puts("[%s]: [%s], App: [%s]: PWD: [%s]]" % [Time.now, gem_name, $0, ENV['PWD']])
     end
   end
 
-  track!
+  track! self.name
 end
